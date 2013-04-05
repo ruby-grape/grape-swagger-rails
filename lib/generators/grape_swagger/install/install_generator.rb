ae42f42aa642c30f8e15c03fdd6e100827dfb5c9
Aleksandr Ivanov <radanisk@ya.ru>
2013-04-05 16:58:21 +0400

    Removed route and general_api template

--- lib/generators/grape_swagger/install/install_generator.rb
+++ lib/generators/grape_swagger/install/install_generator.rb
@@ -9,18 +9,24 @@ module GrapeSwagger
       end
 
       def install
-        Zip::ZipFile.foreach(open('https://github.com/wordnik/swagger-ui/archive/master.zip')) do |zip_entry|
-          zip_entry.extract(zip_entry.to_s.gsub('swagger-ui-master/dist/', 'public/apidoc/')) if zip_entry.to_s =~ /\/dist\//
+        install_path = 'apidoc'
+        swagger_ui_repo = 'https://github.com/wordnik/swagger-ui/'
+        api_path = 'api'
+
+        Zip::ZipFile.foreach(open("#{swagger_ui_repo}/archive/master.zip")) do |zip_entry|
+          zip_entry.extract(zip_entry.to_s.gsub('swagger-ui-master/dist/', "public/#{install_path}/")) if zip_entry.to_s =~ /\/dist\//
         end
 
-        file_name = 'public/apidoc/index.html'
+        file_name = "public/#{install_path}/index.html"
         text = File.read(file_name)
-        new_text = text.gsub('css/', '/apidoc/css/').gsub('lib/', '/apidoc/lib/').gsub('swagger-ui.js', '/apidoc/swagger-ui.js').gsub('images/', '/apidoc/images/').gsub('supportHeaderParams: false', 'supportHeaderParams: true').gsub('http://petstore.swagger.wordnik.com/api/api-docs.json', '/api/swagger_doc.json')
+        new_text = text.
+            gsub('css/', "/#{install_path}/css/").
+            gsub('lib/', "/#{install_path}/lib/").
+            gsub('swagger-ui.js', "/#{install_path}/swagger-ui.js").
+            gsub('images/', "/#{install_path}/images/").
+            gsub('supportHeaderParams: false', 'supportHeaderParams: true').
+            gsub('http://petstore.swagger.wordnik.com/api/api-docs.json', "/#{api_path}/swagger_doc.json")
         File.open(file_name, 'w') { |file| file.puts new_text }
-
-        template 'general_api.rb', 'app/api/general_api.rb'
-
-        route "mount GeneralAPI => '/'"
       end
     end
   end
