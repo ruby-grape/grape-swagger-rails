b58a0257dec46769965eb8f59c80015225c861c4
Danil Pismenny <danil@orionet.ru>
2013-07-16 17:05:30 +0400

    Support custom headers

--- lib/grape-swagger-rails.rb
+++ lib/grape-swagger-rails.rb
@@ -1,10 +1,11 @@
 require "grape-swagger-rails/engine"
 
 module GrapeSwaggerRails
-  mattr_accessor :discoveryUrl, :apiKey
+  mattr_accessor :discoveryUrl, :apiKey, :headers
 
   self.discoveryUrl = '/swagger_doc.json'
   self.apiKey = 'special-key'
+  self.headers = {}
 
 end
 
