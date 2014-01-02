023be4c24fb9412a7d9953190e0020913463ca39
Joel Van Horn <joel@joelvanhorn.com>
2014-01-02 15:23:13 -0500

    Added ability to specify authentication block for controller actions

--- app/controllers/grape_swagger_rails/application_controller.rb
+++ app/controllers/grape_swagger_rails/application_controller.rb
@@ -1,8 +1,12 @@
 module GrapeSwaggerRails
   class ApplicationController < ActionController::Base
-    #layout :application
+    before_filter do
+      if GrapeSwaggerRails.options.authentication_proc
+        instance_exec(request, &GrapeSwaggerRails.options.authentication_proc)
+      end
+    end
+    
     def index
-
     end
   end
 end
--- lib/grape-swagger-rails.rb
+++ lib/grape-swagger-rails.rb
@@ -1,19 +1,23 @@
 require "grape-swagger-rails/engine"
 
 module GrapeSwaggerRails
-  class Options < Struct.new(:url, :api_key_name, :api_key_type, :api_auth, :headers, :app_name, :app_url)
+  class Options < Struct.new(:url, :api_key_name, :api_key_type, :api_auth, :headers, :app_name, :app_url, :authentication_proc)
+    def authenticate_with(&block)
+      self.authentication_proc = block
+    end
   end
   
   mattr_accessor :options
   
   self.options = Options.new(
-    url:          '/swagger_doc.json',
-    api_key_name: 'api_key',
-    api_key_type: 'query',
-    api_auth:     '', # 'basic'
-    headers:      {},
-    app_name:     'Swagger',
-    app_url:      'http://swagger.wordnik.com'
+    url:                  '/swagger_doc.json',
+    api_key_name:         'api_key',
+    api_key_type:         'query',
+    api_auth:             '', # 'basic'
+    headers:              {},
+    app_name:             'Swagger',
+    app_url:              'http://swagger.wordnik.com',
+    authentication_proc:  nil # Proc used as a controller before filter that returns a boolean
   )
   
 end
