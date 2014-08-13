3dcf99e81544cacc59310f6b1d2f6f8567283d61
shinn <shinn@tinkerbox.com.sg>
2014-08-13 21:15:29 +0800

    Add bearer support

--- lib/grape-swagger-rails.rb
+++ lib/grape-swagger-rails.rb
@@ -6,23 +6,22 @@ module GrapeSwaggerRails
       self.authentication_proc = block
     end
   end
-  
+
   mattr_accessor :options
-  
+
   self.options = Options.new(
-    
+
     url:                  '/swagger_doc.json',
     app_name:             'Swagger',
     app_url:              'http://swagger.wordnik.com',
-    
+
     headers:              {},
-    
-    api_auth:             '',        # 'basic'
+
+    api_auth:             '',        # 'basic' or 'bearer'
     api_key_name:         'api_key', # 'Authorization'
     api_key_type:         'query',   # 'header'
-    
+
     authentication_proc:  nil # Proc used as a controller before filter that returns a boolean
   )
-  
-end
 
+end
