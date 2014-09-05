c76b0163966aa5e35abcd503ecd8d169a9c9d419
dblock <dblock@dblock.org>
2014-09-05 09:33:40 -0400

    Fix: make the dummy app runnable.

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -4,7 +4,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 
+require 'sass'
 require 'grape'
 require 'grape-swagger'
-require 'sass'
 require 'coffee_script'
