0fc9b15ecc420244982e2588cc59dce699d5f084
dblock <dblock@dblock.org>
2014-11-28 12:36:47 -0500

    Fix: compatibility with grape-swagger 0.8.0 and HEAD.

--- spec/dummy/config/initializers/swagger.rb
+++ spec/dummy/config/initializers/swagger.rb
@@ -1,3 +1,3 @@
-GrapeSwaggerRails.options.url = '/api/swagger_doc.json'
+GrapeSwaggerRails.options.url = '/api/swagger_doc'
 GrapeSwaggerRails.options.app_name = 'Swagger'
 GrapeSwaggerRails.options.app_url  = 'http://localhost:3000'
