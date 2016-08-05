041595fed53496161d0e9b11a00c4827de33da6f
pezholio <pezholio@gmail.com>
2016-08-05 16:39:40 +0100

    Update from dist

--- app/assets/javascripts/grape_swagger_rails/application.js
+++ app/assets/javascripts/grape_swagger_rails/application.js
@@ -4,12 +4,9 @@
 //= require ./jquery.ba-bbq.min
 //= require ./handlebars-2.0.0
 //= require ./marked
-//= require ./lodash.min
+//= require ./underscore-min
 //= require ./backbone-min
 //= require ./swagger-ui.min
-//= require ./highlight.9.1.0.pack
-//= require ./js-yaml.min
-//= require ./jsoneditor.min
-//= require ./object-assign-pollyfill
+//= require ./highlight.7.3.pack
 //= require ./swagger-oauth
 //= require ./base64
