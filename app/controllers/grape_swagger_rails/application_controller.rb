083b19a2d764a1d9da7e2bb51b87f565cdfecc7b
serggl <sergey.glukhov@gmail.com>
2016-09-21 15:37:30 +0300

    added Rails version check to use a proper callback: before_action or before_filter

--- app/controllers/grape_swagger_rails/application_controller.rb
+++ app/controllers/grape_swagger_rails/application_controller.rb
@@ -1,11 +1,20 @@
 module GrapeSwaggerRails
   class ApplicationController < ActionController::Base
-    before_action do
-      callback = [GrapeSwaggerRails.options.before_action, GrapeSwaggerRails.options.before_filter].compact.first
-      instance_exec(request, &callback) if callback
+
+    if Rails::VERSION::MAJOR >= 4
+      before_action { run_before_action }
+    else
+      before_filter { run_before_action }
     end
 
     def index
+    end
+
+    private
+
+    def run_before_action
+      callback = [GrapeSwaggerRails.options.before_action, GrapeSwaggerRails.options.before_filter].compact.first
+      instance_exec(request, &callback) if callback
     end
   end
 end
